18739d78bedaffc20da7e6062d20f865d3ed69f8
Kenec <you@example.com>
2017-08-24 11:47:54 +0100

    chore(karma.config) setup the unit testing for front end
    - install the dependencies for the front end test
    - write a mock test
    
    [(Delivers) #150398701]

---
+++ test/front_end/sample.test.js
@@ -0,0 +1,7 @@
+// A dummy test 
+describe('Arithmetic Test', function() {
+    it('should add two numbers', function() {
+        // expect 2 + 2 to add 4
+        expect(2 + 2).toEqual(4);
+    });
+  });
