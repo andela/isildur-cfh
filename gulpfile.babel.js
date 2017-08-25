41c7bb7acec3f8c6f1117f6f03b91b77f34b8309
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-08-25 15:24:50 +0100

    chore:(test): change-test-script
    - delete grunt file
    - change test script to gulp-test
    
    delivers #150398703

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -25,7 +25,7 @@ gulp.task('jasmine', () => {
     .pipe(plugins.jasmine());
 });
 
-gulp.task('test', () => gulp.src(['./test/game/game.js', './test/user/model.js'], { read: false })
+gulp.task('test', () => gulp.src(['./test/user/model.js', './test/game/game.js'], { read: false })
   .pipe(plugins.coverage.instrument({
     pattern: ['**/test*'],
     debugDirectory: 'debug'
--- test/user/model.js
+++ test/user/model.js
@@ -23,25 +23,18 @@
 //             done();
 //         });
 
-//         describe('Method Save', function() {
-//             // it('should be able to save whithout problems', function(done) {
-//             //     return user.save(function(err) {
-//             //         should.not.exist(err);
-//             //         done();
-//             //     });
-//             // });
+        describe('Method Save', function() {
+            it('should be able to show an error when try to save witout name', function(done) {
+                user.name = '';
+                return user.save(function(err) {
+                    should.exist(err);
+                    done();
+                });
+            });
+        });
 
-//             it('should be able to show an error when try to save witout name', function(done) {
-//                 user.name = '';
-//                 return user.save(function(err) {
-//                     should.exist(err);
-//                     done();
-//                 });
-//             });
-//         });
-
-//         after(function(done) {
-//             done();
-//         });
-//     });
-// });
+        after(function(done) {
+            done();
+        });
+    });
+});
