3f5e36675d3861a15391e066aedae13ef9691a80
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-08-28 18:05:50 +0100

    fix test

--- test/user/model.js
+++ test/user/model.js
@@ -24,7 +24,6 @@ describe('<Unit Test>', function() {
         });
 
         describe('Method Save', function() {
-<<<<<<< HEAD
             // it('should be able to save whithout problems', function(done) {
             //     return user.save(function(err) {
             //         should.not.exist(err);
@@ -41,17 +40,6 @@ describe('<Unit Test>', function() {
             });
         });
 
-=======
-            it('should be able to show an error when try to save witout name', function(done) {
-                user.name = '';
-                return user.save(function(err) {
-                    should.exist(err);
-                    done();
-                });
-            });
-        });
-
->>>>>>> chore:(test): change-test-script
         after(function(done) {
             done();
         });
