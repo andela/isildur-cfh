0d6b0d8a7fb4d671d8b660f1ffcb4eb60eecc455
jattoabdul <jattoade@gmail.com>
2017-08-28 12:45:56 +0100

    feat(db config): fix the heroku db error -ensure heroku app deploys and runs well without error my changing the db url env name and value to use heroku mlab add-ons [Fixes #150398707]

--- config/env/all.js
+++ config/env/all.js
@@ -6,5 +6,5 @@ var keys = rootPath + '/keys.txt';
 module.exports = {
 	root: rootPath,
 	port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL
+    db: process.env.MONGOHQ_URL || process.env.MONGODB_URI
 };
