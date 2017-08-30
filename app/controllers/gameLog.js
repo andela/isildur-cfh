29952e6937fc20699d9f819e347e15c7baaeeb76
Kenec <you@example.com>
2017-08-30 14:54:25 +0100

    fix(gameLog): modify gameLog
    - add respose to gameLog controller
    
    [Fixes #150398709]

--- app/controllers/gameLog.js
+++ app/controllers/gameLog.js
@@ -13,6 +13,13 @@ _ = require('underscore');
 exports.create = function(req, res) {
     if (req.params.id && req.body.winner && req.body.players && req.body.rounds) {
         let gamelog = new GameLog(req.body);
-        
+        gamelog.save((err) => {
+            res.status(400).send({
+                message: 'Cannot save gamelog'
+            });
+            res.status(200).send({
+                message: 'Gamelog saved successfully!'
+            });
+        })
     }
   };
