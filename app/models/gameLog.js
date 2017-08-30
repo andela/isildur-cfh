c33e1a554d131ee2e0224702fd8a2ab5ea34a596
Kenec <you@example.com>
2017-08-30 14:22:19 +0100

    feature(gameLog): create gameLog model - create a gameLog Model [Delivers #150398709]

---
+++ app/models/gameLog.js
@@ -0,0 +1,37 @@
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+config = require('../../config/config'),
+Schema = mongoose.Schema;
+
+/**
+* GameLog Schema
+*/
+var GameLog = new Schema({
+gameId: {
+    type: Number
+},
+winner: {
+    type: String
+},
+players: {
+    type: Array
+},
+rounds: {
+    type: Number
+}
+});
+
+/**
+* Statics
+*/
+GameLog.statics = {
+load: function(id, cb) {
+    this.findOne({
+        id: id
+    }).select('-_id').exec(cb);
+}
+};
+
+mongoose.model('GameLog', GameLog);
