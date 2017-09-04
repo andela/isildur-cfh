086a51ebc3c604fa780dece4d724a940f968f9c2
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-09-04 12:33:39 +0100

    feat(signup): jwt on signup
    - when user successfully signups, he receives a jwt token
    
    [Delivers #150398706]

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,8 +1,33 @@
 angular.module('mean.system')
   .controller('IndexController',
-    ['$scope', 'Global', '$location', 'socket', 'game', 'AvatarService',
-      ($scope, Global, $location, socket, game, AvatarService) => {
+    ['$scope', 'Global', '$location', 'socket', 'game', 'AvatarService', '$http', '$window',
+      ($scope, Global, $location, socket, game, AvatarService, $http, $window) => {
         $scope.global = Global;
+
+        $scope.errorMessage = '';
+
+        $scope.signup = () => {
+          if (
+            $scope.name &&
+            $scope.name.length > 0 &&
+            $scope.email &&
+            $scope.password
+          ) {
+            const newUser = {
+              name: $scope.name,
+              password: $scope.password,
+              email: $scope.email
+            };
+
+            $http.post('/api/auth/signup', newUser)
+              .then((response) => {
+                $window.localStorage.setItem('token', response.data.token);
+                $location.path('/#!/');
+              }, (err) => {
+                $location.search(`error=${err.data.error}`);
+              });
+          }
+        };
 
         $scope.playAsGuest = () => {
           game.joinGame();
