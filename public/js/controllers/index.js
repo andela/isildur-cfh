b549a723cef0b8347259d0ed8630d7ff66d6df36
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-09-07 09:11:32 +0100

    feat(signup): jwt on signup
    - fix hound errors
    
    [Finishes #150398706]

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,15 +1,15 @@
 angular.module('mean.system')
-.controller('IndexController',
-[
-  '$scope',
-  'Global',
-  '$location',
-  'socket',
-  'game',
-  'AvatarService',
-  '$http',
-  '$window',
-  (
+  .controller('IndexController',
+  [
+    '$scope',
+    'Global',
+    '$location',
+    'socket',
+    'game',
+    'AvatarService',
+    '$http',
+    '$window',
+    (
       $scope,
       Global,
       $location,
@@ -21,31 +21,31 @@ angular.module('mean.system')
     ) => {
       $scope.global = Global;
 
-    $scope.errorMessage = '';
+      $scope.errorMessage = '';
 
-    $scope.signup = () => {
-      if (
+      $scope.signup = () => {
+        if (
           $scope.name &&
-          $scope.name.length > 0 &&
-          $scope.email &&
-          $scope.password
+        $scope.name.length > 0 &&
+        $scope.email &&
+        $scope.password
         ) {
-        const newUser = {
-          name: $scope.name,
-          password: $scope.password,
-          email: $scope.email
-        };
+          const newUser = {
+            name: $scope.name,
+            password: $scope.password,
+            email: $scope.email
+          };
 
-        $http.post('/api/auth/signup', newUser)
-        .then((response) => {
-          $window.localStorage.setItem('token', response.data.token);
-          $location.path('/#!/');
-          $window.location.reload();
-        }, (err) => {
-          $location.search(`error=${err.data.error}`);
-        });
-      }
-    };
+          $http.post('/api/auth/signup', newUser)
+            .then((response) => {
+              $window.localStorage.setItem('token', response.data.token);
+              $location.path('/#!/');
+              $window.location.reload();
+            }, (err) => {
+              $location.search(`error=${err.data.error}`);
+            });
+        }
+      };
 
       $scope.playAsGuest = () => {
         game.joinGame();
