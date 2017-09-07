00287f6842461a8a5d4172c008f4d90f5eef5c66
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-09-07 09:11:32 +0100

    feat(signup): jwt on signup
    - fix hound errors
    
    [Finishes #150398706]

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,67 +1,67 @@
 angular.module('mean.system')
-.controller('IndexController',
-[
-  '$scope',
-  'Global',
-  '$location',
-  'socket',
-  'game',
-  'AvatarService',
-  '$http',
-  '$window',
-  (
-      $scope,
-      Global,
-      $location,
-      socket,
-      game,
-      AvatarService,
-      $http,
-      $window
-    ) => {
-      $scope.global = Global;
+  .controller('IndexController',
+    [
+      '$scope',
+      'Global',
+      '$location',
+      'socket',
+      'game',
+      'AvatarService',
+      '$http',
+      '$window',
+      (
+        $scope,
+        Global,
+        $location,
+        socket,
+        game,
+        AvatarService,
+        $http,
+        $window
+      ) => {
+        $scope.global = Global;
 
-    $scope.errorMessage = '';
+        $scope.errorMessage = '';
 
-    $scope.signup = () => {
-      if (
-          $scope.name &&
+        $scope.signup = () => {
+          if (
+            $scope.name &&
           $scope.name.length > 0 &&
           $scope.email &&
           $scope.password
-        ) {
-        const newUser = {
-          name: $scope.name,
-          password: $scope.password,
-          email: $scope.email
+          ) {
+            const newUser = {
+              name: $scope.name,
+              password: $scope.password,
+              email: $scope.email
+            };
+
+            $http.post('/api/auth/signup', newUser)
+              .then((response) => {
+                $window.localStorage.setItem('token', response.data.token);
+                $location.path('/#!/');
+                $window.location.reload();
+              }, (err) => {
+                $location.search(`error=${err.data.error}`);
+              });
+          }
         };
 
-        $http.post('/api/auth/signup', newUser)
-        .then((response) => {
-          $window.localStorage.setItem('token', response.data.token);
-          $location.path('/#!/');
-          $window.location.reload();
-        }, (err) => {
-          $location.search(`error=${err.data.error}`);
-        });
-      }
-    };
+        $scope.playAsGuest = () => {
+          game.joinGame();
+          $location.path('/app');
+        };
 
-      $scope.playAsGuest = () => {
-        game.joinGame();
-        $location.path('/app');
-      };
+        $scope.showError = () => {
+          if ($location.search().error) {
+            return $location.search().error;
+          }
+          return false;
+        };
 
-      $scope.showError = () => {
-        if ($location.search().error) {
-          return $location.search().error;
-        }
-        return false;
-      };
-
-      $scope.avatars = [];
-      AvatarService.getAvatars()
-        .then((data) => {
-          $scope.avatars = data;
-        });
-    }]);
+        $scope.avatars = [];
+        AvatarService.getAvatars()
+          .then((data) => {
+            $scope.avatars = data;
+          });
+      }]);
