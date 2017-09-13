8137dd689b9bc2b620d12206d260aa2b20ed26c4
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-09-13 18:28:22 +0100

    chore(coveralls): integrate-coveralls-service - modify test to reflect changes made to features
    - add test script to package.json
    
    - add coveralls task to gulp file
    
    - add coveralls badge to readme
    
    - add node version to travis.yml file
    
    delivers #150398704

--- test/user/model.js
+++ test/user/model.js
@@ -1,47 +1,45 @@
 /**
  * Module dependencies.
  */
-var should = require('should'),
-    app = require('../../server'),
-    mongoose = require('mongoose'),
-    User = mongoose.model('User');
+let should = require('should'),
+  app = require('../../server'),
+  mongoose = require('mongoose'),
+  User = mongoose.model('User');
 
-//Globals
-var user;
+// Globals
+let user;
 
-//The tests
-describe('<Unit Test>', function() {
-    describe('Model User:', function() {
-        beforeEach(function(done) {
-            user = new User({
-                name: 'Full name',
-                email: 'test@test.com',
-                username: 'user',
-                password: 'password'
-            });
+// The tests
+describe('<Unit Test>', () => {
+  describe('Model User:', () => {
+    beforeEach((done) => {
+      user = new User({
+        name: 'Full name',
+        email: 'test@test.com',
+        username: 'user',
+        password: 'password'
+      });
 
-            done();
-        });
-
-        describe('Method Save', function() {
-            it('should be able to save whithout problems', function(done) {
-                return user.save(function(err) {
-                    should.not.exist(err);
-                    done();
-                });
-            });
-
-            it('should be able to show an error when try to save witout name', function(done) {
-                user.name = '';
-                return user.save(function(err) {
-                    should.exist(err);
-                    done();
-                });
-            });
-        });
-
-        afterEach(function(done) {
-            done();
-        });
+      done();
     });
+
+    describe('Method Save', () => {
+      it('should be able to save whithout problems', done => user.save((err) => {
+        should.not.exist(err);
+        done();
+      }));
+
+      it('should be able to show an error when try to save witout name', (done) => {
+        user.name = '';
+        return user.save((err) => {
+          should.exist(err);
+          done();
+        });
+      });
+    });
+
+    afterEach((done) => {
+      done();
+    });
+  });
 });
