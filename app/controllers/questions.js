b3475392fc764d8f30e96235b4fed1e01d13a47a
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-09-13 15:02:08 +0100

    more hounf fix

--- app/controllers/questions.js
+++ app/controllers/questions.js
@@ -2,42 +2,45 @@
  * Module dependencies.
  */
 const mongoose = require('mongoose'),
-    async = require('async'),
-    Question = mongoose.model('Question'),
-    _ = require('underscore');
+  async = require('async'),
+  Question = mongoose.model('Question'),
+  _ = require('underscore');
 
 
 /**
  * Find question by id
  */
 exports.question = (req, res, next, id) => {
-    Question.load(id, (err, question) => {
-        if (err) return next(err);
-        if (!question) return next(new Error('Failed to load question ' + id));
-        req.question = question;
-        next();
-    });
+  Question.load(id, (err, question) => {
+    if (err) return next(err);
+    if (!question) return next(new Error('Failed to load question ' + id));
+    req.question = question;
+    next();
+  });
 };
 
 /**
  * Show an question
  */
 exports.show = (req, res) => {
-    res.jsonp(req.question);
+  res.jsonp(req.question);
 };
 
 /**
  * List of Questions
  */
 exports.all = (req, res) => {
-    Question.find({ official: true, numAnswers: { $lt: 3 } }).select('-_id').exec((err, questions) => {
-      if (err) {
-          res.render('error', {
-              status: 500
-          });
-      } else {
-          res.jsonp(questions);
-      }
+  Question.find({
+    official: true,
+    numAnswers: { $lt: 3 }
+  }).select('-_id').exec((err, questions) => {
+    if (err) {
+      res.render('error', {
+        status: 500
+      });
+    } else {
+      res.jsonp(questions);
+    }
   });
 };
 
@@ -45,14 +48,15 @@ exports.all = (req, res) => {
  * List of Questions (for Game class)
  */
 exports.allQuestionsForGame = (region, cb) => {
-    Question.find({ official: true,
-      numAnswers: { $lt: 3 },
-      region
-    }).select('-_id').exec((err, questions) => {
-        if (err) {
-            console.log(err);
-        } else {
-            cb(questions);
-        }
-    });
+  Question.find({
+    official: true,
+    numAnswers: { $lt: 3 },
+    region
+  }).select('-_id').exec((err, questions) => {
+    if (err) {
+      console.log(err);
+    } else {
+      cb(questions);
+    }
+  });
 };
