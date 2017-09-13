f34f9b9fec0ecf1ef1066f6c125a981268191a45
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-09-13 17:58:06 +0100

    hound fix underway

--- app/controllers/questions.js
+++ app/controllers/questions.js
@@ -3,8 +3,8 @@
  */
 const mongoose = require('mongoose'),
   async = require('async'),
-  Question = mongoose.model('Question'),
-  _ = require('underscore');
+  Question = mongoose.model('Question');
+  // _ = require('underscore');
 
 
 /**
